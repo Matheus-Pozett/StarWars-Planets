acb982dab8bf74cb7a8b7904bb9c876673586dac
Matheus Pozett <maa_souza11@hotmail.com>
2025-06-19 12:09:42 -0300

    test: corrigi problema nos testes

--- cypress/mocks/fetch.js
+++ cypress/mocks/fetch.js
@@ -3,7 +3,7 @@ import testData from './testData';
 const mockFetch = () => Promise.resolve({
   status: 200,
   ok: true,
-  json: () => Promise.resolve(testData),
+  json: () => Promise.resolve(testData.results),
 });
 
 export default mockFetch;
